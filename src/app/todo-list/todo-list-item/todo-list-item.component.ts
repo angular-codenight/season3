cd6bd3e369d4d9a15e97a659ed0d58df96a0304a
sajjadmaneshi <maneshisajjad@gmail.com>
2024-01-19 16:25:28 +0330

    property binding

--- src/app/todo-list/todo-list-item/todo-list-item.component.ts
+++ src/app/todo-list/todo-list-item/todo-list-item.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   // selector: 'app-todo-list-item',
   // selector: '[app-todo-list-item]', //attribute selector
-  // selector: '.app-todo-list-item', // class selector
+   selector: '.app-todo-list-item', // class selector
   templateUrl: './todo-list-item.component.html',
   styleUrls: ['./todo-list-item.component.scss']
 })
